7453aa6fd03e30a5ab833639fb9b15ebcd48aa70
Kateryna <katyaukr2014@gmail.com>
2026-08-07 18:10:17 +0300

    add header

---
+++ src/js/mobile-menu.js
@@ -0,0 +1,60 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuBtn = document.querySelector('.menu-button');
+    const iconContainer = menuBtn.querySelector('.icon');
+    const modalMenu = document.querySelector('.modal');
+    const menuItem = document.querySelectorAll('.menu-list-item');
+    const header = document.querySelector('.header');
+  
+    let isMenuOpen = false;
+  
+    const burgerIcon = `
+<svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M6 36V33H42V36H6ZM6 25.5V22.5H42V25.5H6ZM6 15V12H42V15H6Z" fill="url(#paint0_radial_1_37)" />
+  <defs>
+    <radialGradient id="paint0_radial_1_37" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(24 24) rotate(90) scale(12 18)">
+      <stop stop-color="#405841" />
+      <stop offset="1" stop-color="#2E433E" />
+    </radialGradient>
+  </defs>
+</svg>
+    `;
+  
+    const crossIcon = `
+<svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M12.8 38L10 35.2L21.2 24L10 12.8L12.8 10L24 21.2L35.2 10L38 12.8L26.8 24L38 35.2L35.2 38L24 26.8L12.8 38Z" fill="#55B887" />
+</svg>
+    `;
+  
+    menuBtn.addEventListener('click', () => {
+      isMenuOpen = !isMenuOpen;
+      iconContainer.innerHTML = isMenuOpen ? crossIcon : burgerIcon;
+      isMenuOpen ? openModal() : closeModal();
+    });
+  
+    menuItem.forEach(item =>
+      item.addEventListener('click', () => {
+        closeModal();
+        iconContainer.innerHTML = burgerIcon;
+        isMenuOpen = false;
+      })
+    );
+  
+    window.addEventListener('scroll', blurHeader);
+  
+    function openModal() {
+      modalMenu.style.display = 'block';
+    }
+  
+    function closeModal() {
+      modalMenu.style.display = 'none';
+    }
+  
+    function blurHeader() {
+      if (!header) return;
+      if (window.scrollY > 60) {
+        header.classList.add('header-scroll');
+      } else {
+        header.classList.remove('header-scroll');
+      }
+    }
+  });
